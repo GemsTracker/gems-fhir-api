887f0cb8720cd9915bf05dd8a87cf4de51e5750d
Jasper van Gestel <jvangestel@gmail.com>
2024-11-14 17:40:22 +0100

    SiteUtil has been deprecated

--- src/Handler/QuestionnaireTaskHandler.php
+++ src/Handler/QuestionnaireTaskHandler.php
@@ -7,19 +7,18 @@ use Gems\Api\Handlers\ModelRestHandler;
 use Gems\Api\Model\ModelApiHelper;
 use Gems\Audit\AuditLog;
 use Gems\Db\ResultFetcher;
-use Gems\Site\SiteUrl;
-use Gems\Site\SiteUtil;
 use Laminas\Db\Adapter\Adapter;
 use Mezzio\Helper\UrlHelper;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Zalt\Base\RequestUtil;
 use Zalt\Loader\ProjectOverloader;
 use Zalt\Model\Data\DataReaderInterface;
 
 class QuestionnaireTaskHandler extends ModelRestHandler
 {
-    protected SiteUrl|null $currentSite = null;
+    protected string|null $currentBaseUrl = null;
 
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
@@ -28,7 +27,6 @@ class QuestionnaireTaskHandler extends ModelRestHandler
         UrlHelper $urlHelper,
         ModelApiHelper $modelApiHelper,
         Adapter $db,
-        protected readonly SiteUtil $siteUtil,
         protected readonly ResultFetcher $resultFetcher,
     ) {
         parent::__construct($eventDispatcher, $auditLog, $loader, $urlHelper, $modelApiHelper, $db);
@@ -37,18 +35,15 @@ class QuestionnaireTaskHandler extends ModelRestHandler
     public function createModel(): DataReaderInterface
     {
         $model = parent::createModel();
-        $currentUrl = null;
-        if ($this->currentSite) {
-            $currentUrl = $this->currentSite->getUrl();
-        }
-        $model->getMetaModel()->addTransformer(new QuestionnaireTaskInfoTransformer($this->resultFetcher, $currentUrl));
+
+        $model->getMetaModel()->addTransformer(new QuestionnaireTaskInfoTransformer($this->resultFetcher, $this->currentBaseUrl));
 
         return $model;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $this->currentSite = $this->siteUtil->getCurrentSite($request);
+        $this->currentBaseUrl = RequestUtil::getCurrentUrl($request);
         return parent::handle($request);
     }
 }
